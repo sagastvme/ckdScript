a51147d66962dd5bb53bb8c7052a19f1cf93cab7
Eduardo <miltormentas@tutanota.com>
2023-10-22 19:22:15 -0400

    added documentdb

--- s3_bucket_demo/lib/s3_bucket_demo-stack.ts
+++ s3_bucket_demo/lib/s3_bucket_demo-stack.ts
@@ -2,7 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as documentDb from 'aws-cdk-lib/aws-docdb';
+import * as docdb from 'aws-cdk-lib/aws-docdb';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 export class S3BucketDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -12,14 +13,21 @@ export class S3BucketDemoStack extends cdk.Stack {
       versioned: true,
       publicReadAccess: true
     });
+    const vpc = new ec2.Vpc(this, 'MyVPC', {   // Create a new VPC
+      maxAzs: 3 // Replace with your desired configuration
+    });
+    const cluster = new docdb.DatabaseCluster(this, 'Database', {
+      masterUser: {
+        username: 'myuser',
+        excludeCharacters: '\"@/:',
+        secretName: '/myapp/mydocdb/masteruser',
+      },
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.MEMORY5, ec2.InstanceSize.LARGE),
+      vpc,
+    });
+    
+    cluster.connections.allowDefaultPortFromAnyIpv4('Open to the world');
 
 
-    const documentDB = new 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3BucketDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
